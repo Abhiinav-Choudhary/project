e46f2829480fcc39c7ea53270eb5e97e9828a934
Abhiinav-Choudhary <abhinavchaudhary30993@gmail.com>
2024-10-18 07:26:49 +0530

    Add files via upload

---
+++ hackthon/jail.js
@@ -0,0 +1,16 @@
+const submit = document.querySelector("#submit")
+     
+submit.onclick= ()=>{
+    let content = document.querySelector(".container")
+    // console.log("hello")
+    content.style.display = "block"
+      content.style.position = "absolute"
+        content.style.top = "25.5%"
+   
+}
+
+
+// const email = document.getElementById("mail")
+// email.display.value = email.display.value.tolowercase()
+
+// const cancel = document.querySelector(".")
---
+++ hackthon/mail.js
@@ -0,0 +1,28 @@
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCUJGKSLgd0qUy6BDqVS_r2WfJF7bpimyo",
+//   authDomain: "contactform-bb044.firebaseapp.com",
+//   databaseURL: "https://contactform-bb044-default-rtdb.firebaseio.com",
+//   projectId: "contactform-bb044",
+//   storageBucket: "contactform-bb044.appspot.com",
+//   messagingSenderId: "180238078542",
+//   appId: "1:180238078542:web:82510053ec7d45b994691f"
+// };
+
+
+//   firebase.initializeApp(firebaseConfig);
+//   var contactformDB = firebase.database().ref('contactform')
+
+//   document.getElementById('submit').addEventListener("submit",submitClick)
+
+//   function submitClick(e){
+//  e.preventDefault();
+//  var name = getElementVal("name");
+//  var email = getElementVal("mail ");
+//  var msgContent = getElementVal("message");
+//  console.log(name,email,msgContent);
+//   }
+
+//   const getElementVal =(id)=>{
+//  return document.getElementById(id).value;
+//   };
